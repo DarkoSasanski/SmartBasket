f27925909dc7c148289cb48f5f7e2fc508d328ec
DarkoSasanski <darko.sasanski@gmail.com>
2023-06-29 22:11:36 +0200

    added static files for admin panel

--- Domasna5/settings.py
+++ Domasna5/settings.py
@@ -113,7 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "data/")
 MEDIA_URL = "/data/"
 
